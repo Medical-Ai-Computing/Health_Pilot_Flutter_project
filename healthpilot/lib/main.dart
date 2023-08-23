cbb5d64be3b15304176a19e51223f5dee9d520d1
Biruk <leoseven59@gmail.com>
2023-08-23 13:05:28 +0300

    SIGNUP & LOGGIN SCREEN WITH PASSWORD RECOVERY SYSTEM = DONE || PAYMENT SCREEN IS PLACED IN PERSONAL INFORMATIONSCREEN.

--- healthpilot/lib/main.dart
+++ healthpilot/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:healthpilot/data/contants.dart';
 
-import 'package:healthpilot/screens/on_boarding_screens/physical_therapy_screen.dart';
-import 'package:healthpilot/screens/on_boarding_screens/signup_and_login_screen.dart';
 
 void main() {
   runApp(const HealthPilotApp());
@@ -20,7 +18,7 @@ class HealthPilotApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Roboto', // Example font family
       ),
-      home: const SignupAndLoginScreen(),
+      home: 
     );
   }
 }
